7071a44f4de2b62b0b0fcca53a3fcc9ef96358f4
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-28 16:33:59 -0300

    dados dos items e das categorias

---
+++ sistema financeiro/src/data/categoria.ts
@@ -0,0 +1,6 @@
+import { Categorias } from '../types/Categorias';
+export const categoria: Categorias = {
+  Comida: { titulo: 'Quentinha', cor: 'blue' },
+  Bebida: { titulo: 'Skol', cor: 'green' },
+  Acessorio: { titulo: 'Roupa Nike', cor: 'brown' },
+};
---
+++ sistema financeiro/src/data/items.ts
@@ -0,0 +1,22 @@
+import { Item } from '../types/Items';
+
+export const items: Item[] = [
+  {
+    data: new Date(),
+    categoria: 'Comida',
+    titulo: 'Quentinha',
+    valor: 15.0,
+  },
+  {
+    data: new Date(),
+    categoria: 'Bebida',
+    titulo: 'Skol',
+    valor: 40.0,
+  },
+  {
+    data: new Date(),
+    categoria: 'Acessório',
+    titulo: 'Roupa Nike',
+    valor: 120,
+  },
+];
