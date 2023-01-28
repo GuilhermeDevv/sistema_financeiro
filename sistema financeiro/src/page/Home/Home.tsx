49c284f7020767f35a8fe24da017983b53272b9b
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-28 15:12:00 -0300

    criação da pagina principal

---
+++ sistema financeiro/src/page/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Content, Header } from './style';
+
+export function Home() {
+  return (
+    <Container>
+      <Content>
+        <Header>
+          <h1>Sistema financeiro</h1>
+        </Header>
+      </Content>
+    </Container>
+  );
+}
---
+++ sistema financeiro/src/page/Home/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Content = styled.div`
+  background-color: #fff;
+`;
+
+export const Header = styled.header`
+  background-color: #808080;
+
+  & h1 {
+    color: ;
+  }
+`;
+
+export const Main = styled.main``;
