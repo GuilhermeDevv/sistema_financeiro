55dbae5d6349c212b61ae9d8deeac6862321e520
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-01 19:58:34 -0300

    estilização das categorias

--- sistema financeiro/src/components/ItemDaTabela/styles.ts
+++ sistema financeiro/src/components/ItemDaTabela/styles.ts
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
+import { colors } from '../../styles/GlobalStyles';
 export const LinhaDaTabela = styled.tr``;
 
 export const ColunaDaTabela = styled.td`
   font-size: clamp(10px, 4vw, 20px);
   font-weight: bold;
+  padding: 0.5rem 0px;
+`;
+export const StylesCategoria = styled.div`
+  background-color: ${colors.secondary};
+  color: ${colors.primary};
+  padding: 0.5rem 1rem;
+  display: inline-block;
+  border-radius: 0.5rem;
 `;
