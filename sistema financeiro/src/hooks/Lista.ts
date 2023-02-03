35f08c9dde6c78f8d9176965ffb187a6115bd7ca
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-02 22:52:51 -0300

    removendo a opcionalidade dos parametros

--- sistema financeiro/src/hooks/Lista.ts
+++ sistema financeiro/src/hooks/Lista.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Item } from '../types/Items';
 
-export function setListaHook(dados?: Item[]) {
+export function setListaHook(dados: Item[]) {
   const [lista, setLista] = useState(dados);
   return { lista, setLista };
 }
