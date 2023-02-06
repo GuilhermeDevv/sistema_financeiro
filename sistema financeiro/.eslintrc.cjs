4783457dc5da9a21f9588e37cf27136283575737
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-06 20:26:36 -0300

    lint

--- sistema financeiro/.eslintrc.cjs
+++ sistema financeiro/.eslintrc.cjs
@@ -1,11 +1,11 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   root: true,
 };
